32c70516bdd83a6b8d049346b242bac52ee009ce
9yuB1t <thsqjarb@gmail.com>
2025-05-14 01:27:46 +0900

    5/13 006~010

---
+++ c010.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    const int kArrarySize = 3;
+
+    int founding[kArrarySize];
+    founding[0] = 918;
+    founding[1] = 1392;
+    founding[2] = 1948;
+
+    cout << "고려 건국 연도 : " << founding[0] << endl;
+    cout << "조선 건국 연도 : " << founding[1] << endl;
+    cout << "한국 건국 연도 : " << founding[2] << endl;
+}
