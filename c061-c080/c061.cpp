7c944fa3b18f285f9cc1a241980c46d293d764a5
9yuB1t <thsqjarb@gmail.com>
2025-05-26 15:29:09 +0900

    05/26 061~065

---
+++ c061-c080/c061.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string sentence = "i like coding";
+
+    sentence.erase(remove(sentence.begin(), sentence.end(), ' '),sentence.end());
+
+    cout << sentence << endl;
+
+    return 0;
+}
