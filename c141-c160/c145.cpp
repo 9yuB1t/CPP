f5da35cdd723353cf11005337377b7cce07c4b0f
9yuB1t <thsqjarb@gmail.com>
2025-07-02 00:10:12 +0900

    06/19 145~147

---
+++ c141-c160/c145.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    pair<string, int> data("백제 근초고왕", 13);
+
+    cout << data.first << " " << data.second << "대왕" << endl;
+
+    return 0;
+}
